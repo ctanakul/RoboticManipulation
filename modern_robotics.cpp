2a61563e5fce0171a89ae364c7946c684ceb4b30
chainatee <c.tanakul@gmail.com>
2018-02-01 08:38:37 -0600

    add so3ToVec

--- modern_robotics.cpp
+++ modern_robotics.cpp
@@ -53,19 +53,41 @@ Eigen::Matrix3d MR::VecToso3(Eigen::Vector3d omg)
   */
   Eigen::Matrix3d mat;
   mat <<
-    0, -omg[2], omg[1],
-    omg[2], 0, -omg[0],
-    -omg[1], omg[0], 0;
+    0, -omg(2), omg(1),
+    omg(2), 0, -omg(0),
+    -omg(1), omg(0), 0;
   return mat;
   
 }
+
+Eigen::Vector3d MR::so3ToVec(Eigen::Matrix3d so3mat)
+{
+  /*
+    Takes a 3x3 skew-symmetric matrix (an element of so(3)).
+    Returns the corresponding vector (angular velocity).
+    Example Input: 
+    so3mat = [[ 0, -3,  2],
+    [ 3,  0, -1],
+    [-2,  1,  0]]
+    Output:
+    [1, 2, 3]
+  */
+  Eigen::Vector3d v;
+  v <<
+    so3mat(2, 1), so3mat(0, 2), so3mat(1, 0);
+  return v;
+  
+}
+
 
 int main()
 {
   std::cout << MR::NearZero(1e-7) << "\n";
   Eigen::Vector3d V(1,2,3);
   Eigen::Matrix3d mat;
-  mat = MR::VecToso3(V);
-  std::cout << mat << std::endl;
+  mat << 0, -3, 2, 3, 0 , -1, -2, 1 , 0;
+  //mat = MR::VecToso3(V);
+  V = MR::so3ToVec(mat);
+  std::cout << V << std::endl;
   return 0;
 }
--- modern_robotics.hpp
+++ modern_robotics.hpp
@@ -8,5 +8,6 @@ namespace MR
 {
   bool NearZero(double);
   Eigen::Matrix3d VecToso3(Eigen::Vector3d);
+  Eigen::Vector3d so3ToVec(Eigen::Matrix3d);
 }
 #endif
